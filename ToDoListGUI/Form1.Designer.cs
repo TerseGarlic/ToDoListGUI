b043ed80816d42c9a018cdfc60d187183ff28a9b
kblagg <kblagg@CVB-579156>
2025-04-29 16:33:39 -0400

    Add project files.

---
+++ ToDoListGUI/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace ToDoListGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxTasks = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // listBoxTasks
+            // 
+            this.listBoxTasks.FormattingEnabled = true;
+            this.listBoxTasks.ItemHeight = 16;
+            this.listBoxTasks.Location = new System.Drawing.Point(12, 0);
+            this.listBoxTasks.Name = "listBoxTasks";
+            this.listBoxTasks.Size = new System.Drawing.Size(576, 260);
+            this.listBoxTasks.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 312);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(135, 312);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(259, 312);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(382, 312);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(497, 312);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 284);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(560, 22);
+            this.textBox1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(600, 347);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBoxTasks);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxTasks;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ ToDoListGUI/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToDoListGUI
+{
+    public partial class Form1 : Form
+    {
+        private List<TaskItem> tasks = new List<TaskItem>();
+        private string filePath = "tasks.txt";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void AddTask(string description)
+        {
+            if (!string.IsNullOrWhiteSpace(description))
+            {
+                tasks.Add(new TaskItem(description));
+                UpdateTaskList();
+            }
+        }
+
+        private void UpdateTaskList()
+        {
+            listBoxTasks.Items.Clear();
+            foreach (var task in tasks)
+            {
+                string status = task.IsCompleted ? "[X]" : "[ ]";
+                listBoxTasks.Items.Add($"{status} {task.Description}");
+            }
+        }
+
+        private void MarkSelectedTaskAsCompleted()
+        {
+            int index = listBoxTasks.SelectedIndex;
+            if (index >= 0 && index < tasks.Count)
+            {
+                tasks[index].IsCompleted = true;
+                UpdateTaskList();
+            }
+        }
+
+        private void RemoveSelectedTask()
+        {
+            int index = listBoxTasks.SelectedIndex;
+            if (index >= 0 && index < tasks.Count)
+            {
+                tasks.RemoveAt(index);
+                UpdateTaskList();
+            }
+        }
+
+        private void SaveTasks()
+        {
+            List<string> lines = new List<string>();
+            foreach (var task in tasks)
+                lines.Add($"{task.Description}|{task.IsCompleted}");
+            File.WriteAllLines(filePath, lines);
+        }
+
+        private void LoadTasks()
+        {
+            if (File.Exists(filePath))
+            {
+                tasks.Clear();
+                foreach (var line in File.ReadAllLines(filePath))
+                {
+                    string[] parts = line.Split('|');
+                    var task = new TaskItem(parts[0]);
+                    if (parts.Length > 1 && bool.TryParse(parts[1], out bool done))
+                        task.IsCompleted = done;
+                    tasks.Add(task);
+                }
+                UpdateTaskList();
+            }
+        }
+    }
+}
+public class TaskItem
+{
+    public string Description { get; set; }
+    public bool IsCompleted { get; set; }
+    public TaskItem(string description)
+    {
+        Description = description;
+        IsCompleted = false;
+    }
+}
+
+// Form1.Designer.cs
